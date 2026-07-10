bc90c4ed09dd0fa81c548f10f7cf2fd775336205
ghshhf <hongzhiqian88@gmail.com>
2026-07-11 03:59:34 +0800

    chore: upgrade all dependencies to latest versions

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+﻿import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import vuePlugin from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        NodeJS: 'readonly',
+        __BUILD_TIME__: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      vue: vuePlugin,
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+      'no-undef': 'off',
+      'no-async-promise-executor': 'warn',
+      'no-redeclare': 'warn',
+      'no-control-regex': 'warn',
+      'no-useless-escape': 'warn',
+      'no-empty': 'warn',
+      'no-dupe-keys': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', 'release/**', 'android/**', 'ios/**', 'electron/**', '*.cjs'],
+  },
+];
